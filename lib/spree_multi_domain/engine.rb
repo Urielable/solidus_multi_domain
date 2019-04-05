b11cc9e3f8e3fec8ac95a383ad8d1792500302eb
Jonathan Tapia <jonathan.tapia@magmalabs.io>
2019-04-05 11:23:16 -0600

    Fix issue during API calls returning a layout error

--- lib/spree_multi_domain/engine.rb
+++ lib/spree_multi_domain/engine.rb
@@ -32,13 +32,32 @@ module SpreeMultiDomain
     config.to_prepare &method(:activate).to_proc
 
     initializer "templates with dynamic layouts" do |app|
-      require 'spree_multi_domain/dynamic_template_renderer'
-      ActionView::TemplateRenderer.prepend(SpreeMultiDomain::DynamicTemplateRenderer)
+      ActionView::TemplateRenderer.class_eval do
+        def find_layout_with_multi_store(layout, *args)
+          if @view.respond_to?(:current_store) && @view.current_store && !@view.controller.is_a?(Spree::Admin::BaseController) && !@view.controller.is_a?(Spree::Api::BaseController)
+            store_layout = if layout.is_a?(String)
+                             layout.gsub("layouts/", "layouts/#{@view.current_store.code}/")
+                           else
+                             layout.call.try(:gsub, "layouts/", "layouts/#{@view.current_store.code}/")
+                           end
+
+            begin
+              find_layout_without_multi_store(store_layout, *args)
+            rescue ::ActionView::MissingTemplate
+              find_layout_without_multi_store(layout, *args)
+            end
+          else
+            find_layout_without_multi_store(layout, *args)
+          end
+        end
+
+        alias_method_chain :find_layout, :multi_store
+      end
     end
 
     initializer "current order decoration" do |app|
       require 'spree/core/controller_helpers/order'
-      ::Spree::Core::ControllerHelpers::Order.prepend(Module.new do
+      ::Spree::Core::ControllerHelpers::Order.module_eval do
         def current_order_with_multi_domain(options = {})
           options[:create_order_if_necessary] ||= false
           current_order_without_multi_domain(options)
@@ -49,7 +68,8 @@ module SpreeMultiDomain
 
           @current_order
         end
-      end)
+        alias_method_chain :current_order, :multi_domain
+      end
     end
 
     initializer 'spree.promo.register.promotions.rules' do |app|
